5920df259e5b960d551551d83dfb85c10e41d1f0
syedfarhanreza <syedfarhanreza.pub16@gmail.com>
2022-10-26 02:04:59 +0600

    courses data get and send

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
 
+const courses = require('./data/courses.json');
+
 app.get('/', (req,res) => {
     res.send('elearning api running');
 });
+app.get('/courses', (req, res)=> {
+    res.send(courses)
+})
 
 app.listen(port, () =>{
     console.log('OneSchool Server running on port', port);
