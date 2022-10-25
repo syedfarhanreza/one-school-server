948ae241e4051212bdc0197b19a2330e76c9a562
syedfarhanreza <syedfarhanreza.pub16@gmail.com>
2022-10-25 21:51:42 +0600

    index.js file added

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.get('/', (req,res) => {
+    res.send('elearning api running');
+});
+
+app.listen(port, () =>{
+    console.log('OneSchool Server running on port', port);
+})
