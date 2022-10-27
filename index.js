57dd09e870308fd6cbe1bfa7841bdb12bdcbb550
syedfarhanreza <syedfarhanreza.pub16@gmail.com>
2022-10-27 21:58:35 +0600

    vercel added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 const courses = require('./data/courses.json');
 
@@ -8,7 +11,12 @@ app.get('/', (req,res) => {
     res.send('elearning api running');
 });
 app.get('/courses', (req, res)=> {
-    res.send(courses)
+    res.send(courses);
+});
+app.get('/courses/:id',(req, res) =>{
+    const id = req.params.id;
+    const selectedCourse = courses.find(course => course.id === id);
+    res.send(selectedCourse);
 })
 
 app.listen(port, () =>{
